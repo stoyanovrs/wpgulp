64bfe3c9e36990454d3fc34fa08ccc60930e95c0
Rumen Stoyanov <stoyanovrs@gmail.com>
2021-02-21 14:26:32 +0200

    add apollo functions

--- functions.php
+++ functions.php
@@ -172,6 +172,11 @@ require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
+ * Custom AddOns Apollo Theme.
+ */
+require get_template_directory() . '/inc/apollo_functions.php';
+
+/**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
